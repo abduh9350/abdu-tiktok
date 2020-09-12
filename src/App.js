69e3afbd03806a31c9bae65c96a1671558434ba8
Abdu Jemal <abduh9350@gmail.com>
2020-09-11 21:15:30 -0400

    project completed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
-import React from "react";
+import axios from "./axios";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Video from "./Video";
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await axios.get("/v2/posts");
+      setVideos(response.data);
+
+      return response;
+    }
+
+    fetchPosts();
+  }, []);
+
+  console.log(videos);
+
   return (
     <div className="app">
-      <h1>TIKTOK</h1>
       <div className="app_videos">
-        <Video
-          url="https://www.pexels.com/video/a-man-working-with-two-laptops-while-looking-at-notes-stick-on-the-board-behind-3253737/"
-          channel="Abdu"
-          description="tiktok video 1"
-          song="new music"
-          likes={111}
-          messages={222}
-          shares={333}
-        />
-        <Video
-          url="https://www.pexels.com/video/a-man-working-with-two-laptops-while-looking-at-notes-stick-on-the-board-behind-3253737/"
-          channel="Abdu"
-          description="tiktok video 1"
-          song="new music"
-          likes={111}
-          messages={222}
-          shares={333}
-        />
+        {videos.map(
+          ({ url, channel, description, song, likes, messages, shares }) => (
+            <Video
+              url={url}
+              channel={channel}
+              song={song}
+              likes={likes}
+              messages={messages}
+              description={description}
+              shares={shares}
+            />
+          )
+        )}
       </div>
     </div>
   );
